d13ed8bff94825a3529d281eb499f48669c0638e
martyncolmer <martyc1974@hotmail.com>
2020-04-27 11:29:17 +0100

    added linting

--- middleware/processQuestionnaire.test.js
+++ middleware/processQuestionnaire.test.js
@@ -2,35 +2,34 @@ const mockInput = require("./mocks/test-json")
 const processQuestionnaire = require("./processQuestionnaire")
 
 const mockRequest = () => {
-    const req = {
-      files: {
-        jsonFile: {
-            data: JSON.stringify(mockInput)
-        }
+  const req = {
+    files: {
+      jsonFile: {
+        data: JSON.stringify(mockInput)
       }
     }
-    return req
   }
+  return req
+}
 
 const mockResponse = () => {
-    const res = {
-        locals: {}
-    }
-    return res
+  const res = {
+    locals: {}
   }
+  return res
+}
 
 describe("testing processQuestionnaire", () => {
-    let res, req
-    const next = jest.fn()
-  
-    beforeAll(() => {
-        req = mockRequest()
-        res = mockResponse()
-    })
-  
-    it(`should return a json object matching snapshot`, async () => {
-        processQuestionnaire(req, res, next)
-        expect(res.locals.questions).toMatchSnapshot()
-    })
-  
+  let res, req
+  const next = jest.fn()
+
+  beforeAll(() => {
+    req = mockRequest()
+    res = mockResponse()
   })
+
+  it(`should return a json object matching snapshot`, async () => {
+    processQuestionnaire(req, res, next)
+    expect(res.locals.questions).toMatchSnapshot()
+  })
+})
--- server.js
+++ server.js
@@ -1,34 +1,34 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
-const fileUpload = require('express-fileupload');
+const fileUpload = require('express-fileupload')
 const processQuestionnaire = require(`./middleware/processQuestionnaire`)
-const downloadcsv = require(`./middleware/downloadcsv`);
-const bodyParser = require("body-parser");
+const downloadcsv = require(`./middleware/downloadcsv`)
+const bodyParser = require("body-parser")
 
-const app = express();
+const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('static'))
 
 nunjucks.configure("views", {
   autoescape: true,
-  express: app,
-});
+  express: app
+})
 
 app.get('/', function (req, res) {
-    res.render('index.html', { title: "Extract Question Codes and Titles" });
-});
+  res.render('index.html', { title: "Extract Question Codes and Titles" })
+})
 
 app.post('/',
-    fileUpload(),
-    processQuestionnaire,
-    (req, res) => {
-        res.render("results.html", { questions: res.locals.questions, title: "Results" });
-    }
+  fileUpload(),
+  processQuestionnaire,
+  (req, res) => {
+    res.render("results.html", { questions: res.locals.questions, title: "Results" })
+  }
 )
 
 app.post("/download",
   downloadcsv
-);
+)
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.listen(3000, () => console.log('Server started on port 3000'))
