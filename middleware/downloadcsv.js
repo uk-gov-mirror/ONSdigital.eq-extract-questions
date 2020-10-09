e67bb996687b7a96a53fc44168f4f55b73923b7c
Joshua Chapman <joshua.chapman@ons.gov.uk>
2020-10-09 12:02:32 +0100

    Removed tags

--- middleware/downloadcsv.js
+++ middleware/downloadcsv.js
@@ -1,12 +1,30 @@
 const { parse, transforms: { unwind } } = require("json2csv")
 
+// const fields = [
+//   "id",
+//   "title",
+//   "answers.id",
+//   "answers.label",
+//   "answers.options.label",
+//   "answers.options.value"
+// ]
+
 const fields = [
-  "id",
+  "alias",
   "title",
-  "answers.id",
+  "answers.qCode", 
+  "answers.type",
   "answers.label",
-  "answers.options.label",
-  "answers.options.value"
+  "answers.options.label"
+]
+
+const fieldNames = [
+  "Shortcode",
+  "Question Title",
+  "Qcode",
+  "Answers Type",
+  "Answers Label",
+  "Options Label"
 ]
 
 const transforms = [unwind({ paths: ["answers", "answers.options"] })]
@@ -14,5 +32,12 @@ const transforms = [unwind({ paths: ["answers", "answers.options"] })]
 module.exports = (req, res) => {
   const data = parse(JSON.parse(req.body.questions), { fields, transforms })
   res.attachment("questions.csv")
-  res.send(data)
+  // res.send(data)
+
+  const parsedText = data
+    .replace(/(<([^>]+)>)/gi, "")
+    .replace("&#x27;", "'")
+  res.send(parsedText)
+
+
 }
