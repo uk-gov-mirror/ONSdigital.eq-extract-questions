cd1eccd7d84953680fd5b6bb70e05200d032d666
Joshua Chapman <joshua.chapman@ons.gov.uk>
2020-10-12 14:30:51 +0100

    Added custom column names to csv

--- middleware/downloadcsv.js
+++ middleware/downloadcsv.js
@@ -1,30 +1,30 @@
 const { parse, transforms: { unwind } } = require("json2csv")
 
-// const fields = [
-//   "id",
-//   "title",
-//   "answers.id",
-//   "answers.label",
-//   "answers.options.label",
-//   "answers.options.value"
-// ]
-
 const fields = [
-  "alias",
-  "title",
-  "answers.qCode", 
-  "answers.type",
-  "answers.label",
-  "answers.options.label"
-]
-
-const fieldNames = [
-  "Shortcode",
-  "Question Title",
-  "Qcode",
-  "Answers Type",
-  "Answers Label",
-  "Options Label"
+  {
+    label: "Author short code",
+    value: (row) => row.alias
+  },
+  {
+    label: "Question",
+    value: (row) => row.title
+  },
+  {
+    label: "Type",
+    value: (row) => row.answers.type
+  },
+  {
+    label: "Answer label",
+    value: (row) => row.answers.label
+  },
+  {
+    label: "QCode / Answer code",
+    value: (row) => row.answers.qCode
+  },
+  {
+    label: "Transformation information",
+    value: (row) => row.answers.label
+  }
 ]
 
 const transforms = [unwind({ paths: ["answers", "answers.options"] })]
