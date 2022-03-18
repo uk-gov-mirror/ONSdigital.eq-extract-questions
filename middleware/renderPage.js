a9dfc636487561c00da09508bc6b04ae88089c65
martyncolmer <martyc1974@hotmail.com>
2022-03-18 13:57:14 +0000

    added routing and sections

--- middleware/renderPage.js
+++ middleware/renderPage.js
@@ -1,5 +1,14 @@
 module.exports = (page) => {
   return (req, res, next) => {
-    res.render(page, { questions: res.locals.questions, title: res.locals.questionnaire.title, source: res.locals.questionSource })
+    console.log(res.locals.questionnaire.sections)
+    res.render(page, {
+      questions: res.locals.questions,
+      title: res.locals.questionnaire.title,
+      source: res.locals.questionSource,
+      optionLabels: res.locals.optionLabels,
+      sections: res.locals.questionnaire.sections,
+      sectionTitles: res.locals.sectionTitles,
+      questionTitles: res.locals.questionTitles
+    })
   }
 }
