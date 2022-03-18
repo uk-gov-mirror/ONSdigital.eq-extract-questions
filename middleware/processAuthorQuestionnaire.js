7fb20e6694ce1799cdd0fb0c8d03bf7b3deccdba
Martyn Colmer <46242834+martyncolmer@users.noreply.github.com>
2022-03-18 13:57:52 +0000

    added routing and sections (#34)

--- middleware/processAuthorQuestionnaire.js
+++ middleware/processAuthorQuestionnaire.js
@@ -8,8 +8,39 @@ const getAllPages = (questionnaire) => {
   return flatMap(filter(getAllFolders(questionnaire), "pages"), "pages")
 }
 
+const getAllOptionLabels = (questionnaire) => {
+  const pages = getAllPages(questionnaire)
+  const answers = flatMap(filter(pages, "answers"), "answers")
+  const options = flatMap(filter(answers, "options"), "options")
+  const optionLabels = {}
+  options.forEach((option) => {
+    optionLabels[option.id] = option.label
+  })
+  return optionLabels
+}
+
+const getAllSectionTitles = (questionnaire) => {
+  const sectionTitles = {}
+  questionnaire.sections.forEach((section) => {
+    sectionTitles[section.id] = section.title || "undefined title"
+  })
+  return sectionTitles
+}
+
+const getAllQuestionTitles = (questionnaire) => {
+  const pages = getAllPages(questionnaire)
+  const questionTitles = {}
+  pages.forEach((page) => {
+    questionTitles[page.id] = page.title || "undefined title"
+  })
+  return questionTitles
+}
+
 module.exports = (req, res, next) => {
   res.locals.questions = getAllPages(res.locals.questionnaire)
   res.locals.questionSource = "author"
+  res.locals.optionLabels = getAllOptionLabels(res.locals.questionnaire)
+  res.locals.sectionTitles = getAllSectionTitles(res.locals.questionnaire)
+  res.locals.questionTitles = getAllQuestionTitles(res.locals.questionnaire)
   next()
 }
