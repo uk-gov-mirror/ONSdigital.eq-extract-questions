ec0ecae246c577874ae775a3306b2c1909ece40f
martyncolmer <martyc1974@hotmail.com>
2022-03-21 08:50:27 +0000

    removed runner code

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const bodyParser = require("body-parser")
 const setupFilters = require(`./utils/setupFilters`)
 const indexRouter = require(`./routes/index`)
 const authorRouter = require(`./routes/author`)
-const runnerRouter = require(`./routes/runner`)
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true, limit: "10mb" }))
@@ -13,7 +12,6 @@ app.use(bodyParser.json({ limit: "10mb" }))
 app.use(express.static('static'))
 app.use('/', indexRouter)
 app.use('/author', authorRouter)
-app.use('/runner', runnerRouter)
 
 const env = nunjucks.configure("views", {
   autoescape: true,
