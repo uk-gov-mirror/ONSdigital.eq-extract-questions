111fdc71c5a818d49ae46c8ff2bb4fa0d87ad7c5
Martyn Colmer <46242834+martyncolmer@users.noreply.github.com>
2022-03-21 08:55:36 +0000

    Add skips (#36)
    * added skips
    
    * renamed files
    
    * added print styling
    
    * removed runner code

--- middleware/fetchSchema.js
+++ middleware/fetchSchema.js
@@ -2,19 +2,30 @@ const request = require("request-promise-native")
 
 const getQuestionnaireFromURL = async (req, res, next) => {
   try {
-    if (!res.locals.url) {
+    if (!res.locals.urlAWS && !res.locals.urlGCP) {
       throw new Error(`missing url in request`)
     }
     const options = {
       json: true,
-      uri: res.locals.url
+      uri: res.locals.urlAWS
     }
     const response = await request(options)
     res.locals.questionnaire = response
     next()
   }
   catch (e) {
-    res.status(500).send(`Sorry, something went wrong with the url request`)
+    try {
+      const options = {
+        json: true,
+        uri: res.locals.urlGCP
+      }
+      const response = await request(options)
+      res.locals.questionnaire = response
+      next()
+    }
+    catch (e) {
+      res.status(500).send(`Sorry, something went wrong with the url request`)
+    }
   }
 }
 
--- middleware/setUrl.js
+++ middleware/setUrl.js
@@ -1,11 +1,11 @@
 module.exports = (req, res, next) => {
-  const authorUrl = process.env.AUTHOR_SCHEMA_URL || ""
-  const runnerUrl = process.env.RUNNER_SCHEMA_URL || ""
+  const authorUrlAWS = process.env.AUTHOR_SCHEMA_URL_AWS || ""
+  const authorUrlGCP = process.env.AUTHOR_SCHEMA_URL_GCP || ""
   if (req.body.author_qid) {
-    res.locals.url = authorUrl + req.body.author_qid
+    res.locals.urlAWS = authorUrlAWS + req.body.author_qid
   }
-  if (req.body.runner_qid) {
-    res.locals.url = runnerUrl + req.body.runner_qid
+  if (req.body.author_qid) {
+    res.locals.urlGCP = authorUrlGCP + req.body.author_qid
   }
   if (!res.locals.url) {
     res.locals.url = req.body.url || req.query.url
