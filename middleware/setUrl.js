caeefb8aeaf6eda31c15be8535a7739bddb97796
martyncolmer <martyc1974@hotmail.com>
2022-09-20 12:06:04 +0100

    fix typos

--- middleware/setUrl.js
+++ middleware/setUrl.js
@@ -3,10 +3,10 @@ module.exports = (req, res, next) => {
   if (req.body.qid) {
     res.locals.url = authorUrl + req.body.qid
   }
-  if(req.query.qid) {
+  if (req.query.qid) {
     res.locals.url = authorUrl + req.query.qid
   }
-  if(req.query.url) {
+  if (req.query.url) {
     res.locals.url = req.query.url
   }
   next()
